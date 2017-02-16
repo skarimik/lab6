ef217ac74e70ee5700956d60c1e9e529f61063f6
Soheil <skarimik@ucsd.edu>
2017-02-16 15:13:01 -0800

    final

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,9 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	var url = 'project/'+idNumber;
+	$.get(url,callBack);
+	console.log("Url is: "+ url);
 }
 
 /*
@@ -35,4 +38,29 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	var newUrl= "/palette";
+	$.get(newUrl,colors);
+}
+
+/*
+* The call back function
+*/
+function callBack(result){
+	console.log(result);
+	var div = '#project'+result['id']+ " " +".details";
+	
+	console.log(div);
+	$(div).html("<h4> Title: "+result['title']+"</h4>"
+		+ "<img src = '"+result['image']+"'class ='detailsImage'/>"
+		+"<div>Summary: "+result['summary']+"</div>");	
+}
+function colors(result){
+	var colors = result['colors']['hex'];
+	
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+
 }
--- routes/palette.js
+++ routes/palette.js
@@ -3,5 +3,10 @@ var palettes = require('../palettes.json');
 exports.randomPalette = function(req, res) {
 	// get a random palette from the top ones
 	var randomPalette = palettes[Math.floor(palettes.length * Math.random())];
-	res.send('Your random palette is called: ' + randomPalette['title']);
+	//res.send('Your random palette is called: ' + randomPalette['title']);
+	 // of by one, our first project has index 0
+	 console.log(randomPalette);
+  	res.json(randomPalette);
+	
+
 }
